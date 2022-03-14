bc6bfda1d9529e397a970b642c4102fc7613cc89
Antonov Mike <101407994+antonovmike@users.noreply.github.com>
2022-03-14 12:23:23 +0400

    Update exam_1.cs

--- exam_1.cs
+++ exam_1.cs
@@ -4,46 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-    class Person
-        { public string Name { get; }
-        public Person(string name) => Name = name; }
-    class Yo
-        { public string Age { get; }
-        public Yo(string age) => Age = age; }
-    class Mt
-        { public int Math { get; }
-        public Mt(int math) => Math = math; }
-    class Rl
-        { public int Rulang { get; }
-        public Rl(int rulang) => Rulang = rulang; }
-    class Program
-        {
-        static void Main(string[] args)
-        {
-            int amount;
-            string eachName;
-            string eachAge;
-            int eachMath;
-            int eachRulang;
-
-            List<Person> person = new List<Person>() {};
-            List<Yo> age = new List<Yo>() {};
-            List<Mt> math = new List<Mt>() {};
-            List<Rl> rulang = new List<Rl>() {};
-            
-            amount = Convert.ToInt32(Console.ReadLine());
-            
-            for (int i=0; i<amount; i++) {
-                eachName = Console.ReadLine();
-                person.Add(new Person (eachName));
-                eachAge = Console.ReadLine();
-                age.Add(new Yo (eachAge));
-                eachMath = Convert.ToInt32(Console.ReadLine());
-                math.Add(new Mt (eachMath));
-                eachRulang = Convert.ToInt32(Console.ReadLine());
-                rulang.Add(new Rl (eachRulang));
-                Console.WriteLine("{0}. {1} is {2} yo. Math {3}, Russian language {4}", 
-                (i+1), person[i].Name, age[i].Age, math[i].Math, rulang[i].Rulang);
-            }
-        }
+class Person
+    {
+        public string Name;
+        public string Age { get; }
+        public int Math { get; }
+        public int Rulang { get; }
+        public Person(string name, string age, int math, int rulang) => (Name, Age, Math, Rulang) = (name, age, math, rulang);
     }
+    
+class Program
+    {
+// ВЫВОД РЕЗУЛЬТАТА
+        public static void PrintList
+        (List<Person> name, List<Person> age, List<Person> math, List<Person> rulang) {
+            for (int i = 0; i < name.Count; i++)
+            { Console.WriteLine("{0}. {1} is {2} yo. Math {3}, Russian language {4}", 
+            (i+1), name[i].Name, age[i].Age, math[i].Math, rulang[i].Rulang); }
+    }
+    static void Main(string[] args)
+    {
+        int amount;
+        string eachName;
+        string eachAge;
+        int eachMath;
+        int eachRulang;
+
+        List<Person> name = new List<Person>() {};
+        List<Person> age = new List<Person>() {};
+        List<Person> math = new List<Person>() {};
+        List<Person> rulang = new List<Person>() {};
+// КОЛИЧЕСТВО СТУДЕНТОВ
+        Console.WriteLine("Enter number of students");
+        amount = Convert.ToInt32(Console.ReadLine());
+// ВВОД ДАННЫХ
+        for (int i=0; i<amount; i++) {
+            Console.WriteLine("Enter a name:");
+            eachName = Console.ReadLine();
+            Console.WriteLine("Enter an age:");
+            eachAge = Console.ReadLine();
+            Console.WriteLine("Enter math grade:");
+            eachMath = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Russian language grade:");
+            eachRulang = Convert.ToInt32(Console.ReadLine());
+            name.Add(new Person (eachName, eachAge, eachMath, eachRulang));
+            age.Add(new Person (eachName, eachAge, eachMath, eachRulang));
+            math.Add(new Person (eachName, eachAge, eachMath, eachRulang));
+            rulang.Add(new Person (eachName, eachAge, eachMath, eachRulang));
+        }
+// ВЫВОД РЕЗУЛЬТАТА
+        PrintList(name, age, math, rulang);
+    }
+}
