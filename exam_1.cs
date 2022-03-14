14e887c808583dd2c35bd88976cd63e8f8023e27
Antonov Mike <101407994+antonovmike@users.noreply.github.com>
2022-03-14 08:10:38 +0400

    Update exam_1.cs

--- exam_1.cs
+++ exam_1.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SoloLearn
-{
     class Person
         { public string Name { get; }
         public Person(string name) => Name = name; }
@@ -49,4 +47,3 @@ namespace SoloLearn
             }
         }
     }
-}
